4bc173fc1bcb9acf07c3ccb0dc8f82d73cf9a1a7
xuanhua.hu <xuanhua.hu@busonline.com>
2018-02-11 12:09:57 +0800

    333

--- src/main/java/com/wondersgroup/springboot/elasticsearch/ESController.java
+++ src/main/java/com/wondersgroup/springboot/elasticsearch/ESController.java
@@ -1,6 +1,7 @@
 package com.wondersgroup.springboot.elasticsearch;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,123 +30,125 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ESController {
 
-	@Autowired
-	private TransportClient client;
-	@GetMapping("/")
-	public String index(){
-		return "index";
-	}
-	
-	@GetMapping("/get/book/novel")
-	public ResponseEntity get(@RequestParam(name="id",defaultValue="")String id){
-		if (id.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		GetResponse result = this.client.prepareGet("book", "novel", id).get();
-		if (!result.isExists()) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity(result.getSource(),HttpStatus.OK);
-	}
-	
-	@PostMapping("/add/book/novel")
-	public ResponseEntity add(
-			@RequestParam String name,
-			@RequestParam String title,
-			@RequestParam("word_count") String wordCount,
-			@RequestParam("publishing_date") @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") Date publishingDate
-			){
-		XContentBuilder json;
-		try {
-			json = XContentFactory.jsonBuilder().startObject();
-			json.field("name", name);
-			json.field("title",title);
-			json.field("word_count", wordCount);
-			json.field("publishing_date", publishingDate.getTime());
-			json.endObject();
-			IndexResponse result = this.client.prepareIndex("book", "novel").setSource(json).get();
-			return new ResponseEntity(result.getId(),HttpStatus.OK);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-		}
-		
-		
-	}
-	
-	@PutMapping("/update/book/novel")
-	public ResponseEntity update(
-			@RequestParam(name="id")String id,
-			@RequestParam(name="name",required=false)String name,
-			@RequestParam(name="title",required=false)String title
-			) {
-		UpdateRequest request=new UpdateRequest("book","novel",id);
-		try {
-			XContentBuilder json = XContentFactory.jsonBuilder().startObject();
-			if (name!=null) {
-				json.field("name", name);
-			}
-			if (title!=null) {
-				json.field("title", title);
-			}
-			json.endObject();
-			request.doc(json);
-			UpdateResponse result = this.client.update(request).get();
-			return new ResponseEntity(result.getResult().toString(),HttpStatus.OK);
+    @Autowired
+    private TransportClient client;
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-	}
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
-	/**
-	 * 删除图书
-	 * @param id
-	 * @return
-	 */
-	@DeleteMapping("delete/book/novel")
-	public ResponseEntity delete(@RequestParam("id")String id){
-		DeleteResponse deleteResponse = this.client.prepareDelete("book", "novel", id).get();
-		return new ResponseEntity(deleteResponse.getResult().toString(),HttpStatus.OK);
-	}
-	
-	@PostMapping("query/book/novel")
-	public ResponseEntity query(
-			@RequestParam(name="name",required=false)String name,
-			@RequestParam(name="title",required=false)String title,
-			@RequestParam(name="gt_word_count",defaultValue="0")int gtWordCuont,
-			@RequestParam(name="lt_word_count",required=false)Integer ltWordCount
-			){
-		BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
-		if (name!=null) {
-			boolQuery.must(QueryBuilders.matchQuery("name", name));
-		}
-		if (title!=null) {
-			boolQuery.must(QueryBuilders.matchQuery("title", title));
-		}
-		RangeQueryBuilder rangQuery = QueryBuilders.rangeQuery("word_count").from(gtWordCuont);
-		if (ltWordCount!=null) {
-			rangQuery.to(ltWordCount);
-		}
-		boolQuery.filter(rangQuery);
-		SearchRequestBuilder builder = this.client.prepareSearch("book")
-		.setTypes("novel")
-		.setQuery(boolQuery)
-		.setFrom(0)
-		.setSize(10);
-		System.out.println(builder);
-		SearchResponse searchResponse = builder.get();
-		List<Map<String, Object>> result=new ArrayList<>();
-		for (SearchHit hit : searchResponse.getHits()) {
-			result.add(hit.getSourceAsMap());
-		}
-		return new  ResponseEntity(result,HttpStatus.OK);
-	}
-	
-	
+    @GetMapping("/get/book/novel")
+    public ResponseEntity get(@RequestParam(name = "id", defaultValue = "") String id) {
+        if (id.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        GetResponse result = this.client.prepareGet("book", "novel", id).get();
+        if (!result.isExists()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(result.getSource(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add/book/novel")
+    public ResponseEntity add(
+            String name,
+            String title,
+            String wordCount
+
+    ) {
+        XContentBuilder json;
+        try {
+            json = XContentFactory.jsonBuilder().startObject();
+            json.field("name", name);
+            json.field("title", title);
+            json.field("word_count", wordCount);
+            json.field("publishing_date", new SimpleDateFormat("yyyy-mm-dd").format(new Date()));
+            json.endObject();
+            IndexResponse result = this.client.prepareIndex("book", "novel").setSource(json).get();
+            return new ResponseEntity(result.getId(), HttpStatus.OK);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+
+    @PutMapping("/update/book/novel")
+    public ResponseEntity update(
+            @RequestParam(name = "id") String id,
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "title", required = false) String title
+    ) {
+        UpdateRequest request = new UpdateRequest("book", "novel", id);
+        try {
+            XContentBuilder json = XContentFactory.jsonBuilder().startObject();
+            if (name != null) {
+                json.field("name", name);
+            }
+            if (title != null) {
+                json.field("title", title);
+            }
+            json.endObject();
+            request.doc(json);
+            UpdateResponse result = this.client.update(request).get();
+            return new ResponseEntity(result.getResult().toString(), HttpStatus.OK);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    /**
+     * 删除图书
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("delete/book/novel")
+    public ResponseEntity delete(@RequestParam("id") String id) {
+        DeleteResponse deleteResponse = this.client.prepareDelete("book", "novel", id).get();
+        return new ResponseEntity(deleteResponse.getResult().toString(), HttpStatus.OK);
+    }
+
+    @PostMapping("query/book/novel")
+    public ResponseEntity query(
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "gt_word_count", defaultValue = "0") int gtWordCuont,
+            @RequestParam(name = "lt_word_count", required = false) Integer ltWordCount
+    ) {
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        if (name != null) {
+            boolQuery.must(QueryBuilders.matchQuery("name", name));
+        }
+        if (title != null) {
+            boolQuery.must(QueryBuilders.matchQuery("title", title));
+        }
+        RangeQueryBuilder rangQuery = QueryBuilders.rangeQuery("word_count").from(gtWordCuont);
+        if (ltWordCount != null) {
+            rangQuery.to(ltWordCount);
+        }
+        boolQuery.filter(rangQuery);
+        SearchRequestBuilder builder = this.client.prepareSearch("book")
+                .setTypes("novel")
+                .setQuery(boolQuery)
+                .setFrom(0)
+                .setSize(10);
+        System.out.println(builder);
+        SearchResponse searchResponse = builder.get();
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (SearchHit hit : searchResponse.getHits()) {
+            result.add(hit.getSourceAsMap());
+        }
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+
 }
 
--- src/main/java/com/wondersgroup/springboot/elasticsearch/config/MyConfiig.java
+++ src/main/java/com/wondersgroup/springboot/elasticsearch/config/MyConfiig.java
@@ -6,18 +6,21 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class MyConfiig {
-
+@Autowired
+	Environment env;
 	@Bean
 	public TransportClient client() throws Exception{
 		// 定义地址端口
 		TransportAddress address=new TransportAddress(InetAddress.getByName("localhost"), 9300);
 		// 定义配置信息
-		Settings setting = Settings.builder().put("cluster.name","timou").build();
+		Settings setting = Settings.builder().put("cluster.name",env.getProperty("cluster.name")).build();
 		// 创建客户端
 		TransportClient client =new PreBuiltTransportClient(setting);
 		client.addTransportAddress(address);
